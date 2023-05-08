8edb86db5e8e8523e26ea647f21782424fa788c1
Nitish Kumar <nitishkumar987654@gmail.com>
2023-05-08 19:43:45 +0530

    recording is working now

--- src/app/recording/recording.component.ts
+++ src/app/recording/recording.component.ts
@@ -1,18 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common'
+import { Location } from '@angular/common';
+import {
+  NgAudioRecorderService,
+  OutputFormat,
+} from '../services/audio-recorder.service';
 
 @Component({
   selector: 'app-recording',
   templateUrl: './recording.component.html',
-  styleUrls: ['./recording.component.scss']
+  styleUrls: ['./recording.component.scss'],
 })
 export class RecordingComponent implements OnInit {
+  outputFormat: OutputFormat = OutputFormat.WEBM_BLOB;
 
-  constructor( private location: Location) { }
+  constructor(
+    private location: Location,
+    private audioRecorderService: NgAudioRecorderService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   back(): void {
-    this.location.back()
-  } 
+    this.location.back();
+  }
+  startRecording() {
+    this.audioRecorderService.startRecording();
+  }
+  stopRecording() {
+    this.audioRecorderService
+      .stopRecording(this.outputFormat)
+      .then((output) => {
+        console.log(output);
+      });
+  }
+
+  pause() {
+    this.audioRecorderService.pause();
+  }
+
+  resume() {
+    this.audioRecorderService.resume();
+  }
+
+  getUserConsent() {
+    this.audioRecorderService.getUserContent();
+  }
+  get recorderState() {
+    return this.audioRecorderService.recorderState;
+  }
 }
