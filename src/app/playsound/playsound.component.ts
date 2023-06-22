36af7d57aeec09a2978f6a072aba6fca1717f44c
nitishriddler <nitishkumar987654@gmail.com>
2023-06-22 21:10:13 +0530

    custom play sound work

--- src/app/playsound/playsound.component.ts
+++ src/app/playsound/playsound.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { AppService } from '../services/app.service';
 import { Observable, tap } from 'rxjs';
 import { NameInfo } from '../models/name-info.model';
+import { AudioService } from '../services/audio.service';
 
 @Component({
   selector: 'app-playsound',
@@ -12,22 +13,44 @@ export class PlaysoundComponent implements OnInit {
   @Input() nameInfo!: NameInfo;
   selectedAudio: any;
   playbackRate: number = 1.0;
+  recordedAudioUrl?: string;
 
-  constructor(private appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private audioService: AudioService
+  ) {}
 
   ngOnInit(): void {
     this.appService.getSelectedAudio().subscribe((x) => {
       this.selectedAudio = x;
     });
+
+    this.getRecordedAudio();
+  }
+
+  getRecordedAudio() {
+    this.audioService
+      .getNameAudio(this.nameInfo.Transliteration!)
+      .subscribe((x) => {
+        if (x) {
+          let recordedAudio = x.blob;
+          this.recordedAudioUrl = URL.createObjectURL(recordedAudio);
+          // this.recordedAudioInUse = x.inUse;
+        }
+      });
   }
 
   playSound() {
-    const path = `/assets/sound/${this.selectedAudio.path}/${this.nameInfo.Audio}`;
     const audio = new Audio();
+    if (this.recordedAudioUrl) {
+      audio.src = this.recordedAudioUrl;
+    } else {
+      const path = `/assets/sound/${this.selectedAudio.path}/${this.nameInfo.Audio}`;
+      audio.src = path;
+    }
+
     audio.playbackRate = this.playbackRate;
-    audio.src = path;
     audio.load();
     audio.play();
   }
- 
 }
